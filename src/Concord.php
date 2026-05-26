0cfce597d94e32291397ff08a82454a7be198ebb
Attila Fulop <1162360+fulopattila122@users.noreply.github.com>
2026-05-26 09:23:58 +0300

    Release v1.18.1

--- src/Concord.php
+++ src/Concord.php
@@ -27,7 +27,7 @@ use Konekt\Concord\Events\RequestWasRegistered;
 
 class Concord implements ConcordContract
 {
-    const VERSION = '1.18.0';
+    const VERSION = '1.18.1';
 
     /** @var Collection  */
     protected $modules;
